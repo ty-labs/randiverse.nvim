1d3e95b0689ba37db965bd82316f237d59ed1d3a
tylowe-labs <tylerlowe@gmail.com>
2023-12-27 13:07:36 -0800

    Add default keymaps + commands to config

--- lua/randiverse/config.lua
+++ lua/randiverse/config.lua
@@ -1,14 +1,70 @@
 local M = {}
 
 M.default_opts = {
+    enabled = true,
+    keymaps_enabled = true,
     keymaps = {
         country = {
             keymap = "<leader>rc",
             command = "country",
             desc = "Generates a random country",
         },
+        datetime = {
+            keymap = "<leader>rd",
+            command = "datetime",
+            desc = "Generates a random datetime",
+        },
+        email = {
+            keymap = "<leader>re",
+            command = "email",
+            desc = "Generates a random email address",
+        },
+        float = {
+            keymap = "<leader>rf",
+            command = "float",
+            desc = "Generates a random float",
+        },
+        hexcolor = {
+            keymap = "<leader>rh",
+            command = "hexcolor",
+            desc = "Generates a random hexcolor",
+        },
+        int = {
+            keymap = "<leader>ri",
+            command = "int",
+            desc = "Generates a random integer",
+        },
+        ip = {
+            keymap = "<leader>rI",
+            command = "ip",
+            desc = "Generates a random ip",
+        },
+        lorem = {
+            keymap = "<leader>rl",
+            command = "lorem",
+            desc = "Generates random lorem ipsum text",
+        },
+        name = {
+            keymap = "<leader>rn",
+            command = "name",
+            desc = "Generates a random name",
+        },
+        url = {
+            keymap = "<leader>ru",
+            command = "url",
+            desc = "Generates a random url",
+        },
+        uuid = {
+            keymap = "<leader>rU",
+            command = "uuid",
+            desc = "Generates a random uuid",
+        },
+        word = {
+            keymap = "<leader>rw",
+            command = "word",
+            desc = "Generates a random word",
+        },
     },
-    enabled = true, -- TODO: Enables setup + register of command
     data = {
         ROOT = (function()
             local path = debug.getinfo(1, "S").source:sub(2)
@@ -109,20 +165,135 @@ M.set_keymaps = function()
             silent = true,
         },
     })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.datetime.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.datetime.command),
+        opts = {
+            desc = M.user_opts.keymaps.datetime.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.email.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.email.command),
+        opts = {
+            desc = M.user_opts.keymaps.email.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.float.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.float.command),
+        opts = {
+            desc = M.user_opts.keymaps.float.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.hexcolor.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.hexcolor.command),
+        opts = {
+            desc = M.user_opts.keymaps.hexcolor.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.int.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.int.command),
+        opts = {
+            desc = M.user_opts.keymaps.int.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.ip.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.ip.command),
+        opts = {
+            desc = M.user_opts.keymaps.ip.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.lorem.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.lorem.command),
+        opts = {
+            desc = M.user_opts.keymaps.lorem.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.name.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.name.command),
+        opts = {
+            desc = M.user_opts.keymaps.name.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.url.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.url.command),
+        opts = {
+            desc = M.user_opts.keymaps.url.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.uuid.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.uuid.command),
+        opts = {
+            desc = M.user_opts.keymaps.uuid.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
+    M.set_keymap({
+        mode = "n",
+        lhs = M.user_opts.keymaps.word.keymap,
+        rhs = string.format(":Randiverse %s<CR>", M.user_opts.keymaps.word.command),
+        opts = {
+            desc = M.user_opts.keymaps.word.desc,
+            noremap = true,
+            silent = true,
+        },
+    })
 end
 
--- TODO: Enable ability to override any command default values to user desire
 M.setup = function(user_opts)
     -- 1st overwrite default options w/ potential user-defined if exist
     M.user_opts = vim.tbl_deep_extend("force", M.default_opts, user_opts or {})
 
-    -- 1st register the command
+    if not M.user_opts.enabled then
+        return
+    end
+
+    -- 2nd register the command
     vim.cmd([[command! -nargs=* Randiverse lua require('randiverse').randiverse({<f-args>})]])
 
-    -- TODO: 2nd register the keymaps which call command w/ args
-    M.set_keymaps()
+    -- 3rd register the keymaps if applicable
+    if M.user_opts.keymaps_enabled then
+        M.set_keymaps()
+    end
 
-    -- 3rd set seed for randomness
+    -- 4th set seed for randomness
     math.randomseed(os.time())
 end
 
