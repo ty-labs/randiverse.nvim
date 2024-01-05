9a9979cf3e8641ab48e5ff6f0a15dab793a7b89f
tylowe-labs <tylerlowe@gmail.com>
2024-01-05 09:07:01 -0800

    Add flag validations to email command

--- lua/randiverse/commands/email.lua
+++ lua/randiverse/commands/email.lua
@@ -17,26 +17,45 @@ local expected_flags = {
     },
     ["digits"] = {
         bool = false,
-        validator = function(s) end,
-        transformer = function(s) end,
+        validator = function(s)
+            if not utils.string_is_non_negative_integer(s) then
+                error(string.format("flag 'digits' can not accept value '%s': value must be a non-negative integer", s))
+            end
+        end,
+        transformer = utils.string_to_integer,
     },
     ["specials"] = {
         bool = false,
-        validator = function(s) end,
-        transformer = function(s) end,
+        validator = function(s)
+            if not utils.string_is_non_negative_integer(s) then
+                error(
+                    string.format("flag 'specials' can not accept value '%s': value must be a non-negative integer", s)
+                )
+            end
+        end,
+        transformer = utils.string_to_integer,
     },
     ["separator"] = {
         bool = true,
     },
     ["muddle-property"] = {
         bool = false,
-        validator = utils.string_is_probability,
+        validator = function(s)
+            if not utils.string_is_probability(s) then
+                error(
+                    string.format(
+                        "flag 'muddle-property' can not accept value '%s': value must be in range [0.0, 1.0]",
+                        s
+                    )
+                )
+            end
+        end,
         transformer = utils.string_to_number,
     },
     cross_flags_validator = utils.pass_through,
 }
 
--- TODO: Probability that first/last name is substring + first/last name positions odds
+-- TODO: Probability that first/last name is substring + first/last name positions odds?
 local generate_username = function(flags)
     local first_name = utils.read_random_line(config.user_opts.data.ROOT .. config.user_opts.data.name.FIRST)
     local last_name = utils.read_random_line(config.user_opts.data.ROOT .. config.user_opts.data.name.LAST)
@@ -55,10 +74,10 @@ local generate_username = function(flags)
     end
 
     local digit_list = config.user_opts.data.email.digits
-    local special_list = config.user_opts.data.email.specials
     for _ = 1, flags["digits"] or config.user_opts.data.email.default_digits do
         table.insert(username_components, digit_list[math.random(#digit_list)])
     end
+    local special_list = config.user_opts.data.email.specials
     for _ = 1, flags["specials"] or config.user_opts.data.email.default_specials do
         table.insert(username_components, special_list[math.random(#special_list)])
     end
